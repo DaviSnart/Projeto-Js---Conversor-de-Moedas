bb4f6f2b560e50df9e6d578903e9db7d305356be
DaviSnart <davi.soares9876543210@gmail.com>
2023-09-11 12:07:06 -0300

    muitas adições de codigos no java script e no html e término do site.

--- web/script.js
+++ web/script.js
@@ -1,14 +1,53 @@
-const convertButton = document.querySelector(".convert-button")
+const convertButton = document.querySelector (".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
-function convertValues () {
-
-    const inputCurrencyValue = document.querySelector(".input-currency").value
+function convertValues (){
+    const inputCurrencyValue =document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector (".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector (".currency-value")
 
     const dolarToday = 5.2
+    const euroToday = 6.2
 
-    const convertedValue = inputCurrencyValue / dolarToday
 
-    console.log (convertedValue)
+    if (currencySelect.value == "dolar"){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }) .format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == "euro"){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE",{
+            style: "currency",
+            currency: "EUR"
+        }) .format(inputCurrencyValue / euroToday)
+        
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }) .format(inputCurrencyValue)// usamos o innerHTML pra pegar algo do html e dar um novo conteúdo a ele
+
+    }
+//TODO ESSE PROJETO ESTÁ DEBAIXO DO CONVERT BUTTON E DA MECÂNICA DE CLICAR
+
+function changeCurrency (){
+    const currencyName = document.getElementById ("currency-name")
+    const currencyImg = document.querySelector (".currency-img")
+
+    if(currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImg.src = "../assets/estados-unidos (1) 1.png"
+    }
+    if(currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "../assets/Design sem nome 3.png"
+    }
+
+    convertValues()
 }
 
-convertButton.addEventListener("click",convertValues)
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
